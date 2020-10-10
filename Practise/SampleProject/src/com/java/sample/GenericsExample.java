d54148625e35b27731f271fd7f45c8aa543b69cd
prashanthkudumula <71508896+prashanthkudumula@users.noreply.github.com>
2020-10-10 20:01:55 +0530

    FourthCommit

--- Practise/SampleProject/src/com/java/sample/GenericsExample.java
+++ Practise/SampleProject/src/com/java/sample/GenericsExample.java
@@ -78,14 +78,6 @@ static void run(){
 		
 		
 	}
-
-
-static void testRun(){
-	
-	System.out.println("Hello world");
-	
-	
-}
 	public static void main(String[] args) {
 
 		// 2.provide type Safety
